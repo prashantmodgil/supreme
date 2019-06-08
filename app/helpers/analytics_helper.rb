ccbc4b177a912e11b20262dbed99815f58e0b4d3
parshantmodgil <prashant080994@gmail.com>
2019-06-08 11:47:49 +0530

    updating complaint status

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -3,19 +3,19 @@ module AnalyticsHelper
     Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).count
   end
   def resolved_complaints
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).where("complaint_status = ?", "resolved" ).count
+    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).where("complaint_status = ?", "resolved"||"ok" ).count
   end
   def pending_complaints
-    Complaint.where("complaint_status != ?", "resolved" ).count
+    Complaint.where("complaint_status != ?", "resolved"||"ok" ).count
   end
   def mec_30 name
     Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).where("mechenic = ?", name ).count
   end
   def mec_solved_30 name
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).where("mechenic = ?", name ).where("complaint_status = ?", "resolved" ).count
+    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).where("mechenic = ?", name ).where("complaint_status = ?", "resolved"||"ok" ).count
   end
   def name_pending_total name
-    Complaint.where("mechenic = ?", name ).where("complaint_status != ?", "resolved" ).count
+    Complaint.where("mechenic = ?", name ).where("complaint_status != ?", "resolved"||"ok" ).count
   end
   def all_mechname
     Mechenic.all.pluck("mec_name")
