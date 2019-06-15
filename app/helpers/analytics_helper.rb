2ae6721cd55248637e849abcd510adb0b0877932
parshantmodgil <prashant080994@gmail.com>
2019-06-15 20:48:09 +0530

    updated searching logic

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,18 +1,18 @@
 module AnalyticsHelper
   def total_complaints
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now).count
+    Complaint.count
   end
   def resolved_complaints
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now).where("complaint_status = ?", "resolved"||"ok" ).count
+    Complaint.where("complaint_status = ?", "resolved"||"ok" ).count
   end
   def pending_complaints
     Complaint.where("complaint_status != ?", "resolved"||"ok" ).count
   end
   def mec_30 name
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now).where("mechenic = ?", name ).count
+    Complaint.where("mechenic = ?", name ).count
   end
   def mec_solved_30 name
-    Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now).where("mechenic = ?", name ).where("complaint_status = ?", "resolved"||"ok" ).count
+    Complaint.where("mechenic = ?", name ).where("complaint_status = ?", "resolved"||"ok" ).count
   end
   def name_pending_total name
     Complaint.where("mechenic = ?", name ).where("complaint_status != ?", "resolved"||"ok" ).count
