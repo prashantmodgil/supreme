32d00e3b430cd222e8abf09301d5d051c590887b
parshantmodgil <prashant080994@gmail.com>
2019-06-17 23:02:24 +0530

    added searching logic according to complaint status

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,14 +1,14 @@
 class Complaint < ApplicationRecord
-#   validates :mechenic,presence: true
-#   validates :createdby,presence: true
-#   validates :dealer,presence: true
-#   validates :product_name,presence: true
-#   validates :complaint_status,presence: true
-#   validates :warranty,presence: true
-#   validates :call_history,presence: true
-#   validates :site_address,presence: true
-#   validates :coustomer_address,presence: true
+  validates :mechenic,presence: true
+  validates :createdby,presence: true
+  # validates :dealer,presence: true
+  validates :product_name,presence: true
+  validates :complaint_status,presence: true
+  validates :warranty,presence: true
+  validates :call_history,presence: true
+  # validates :site_address,presence: true
+  # validates :coustomer_address,presence: true
 #   validates :coustomer_phone,presence: true
-#   validates :coustomer_phone, format: { with: /\d{10}/,message: "only allows 10 digits" }
+  validates :coustomer_phone, format: { with: /\d{10}/,message: "only allows 10 digits" }
    self.per_page = 10
 end
