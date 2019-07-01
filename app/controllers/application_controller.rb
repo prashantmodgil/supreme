d61ec5afa3143c32085d0860dbd254f2257c6adf
parshantmodgil <prashant080994@gmail.com>
2019-07-01 21:00:27 +0530

    mazkiya login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  # http_basic_authenticate_with name: "supreme", password: "password"
+  http_basic_authenticate_with name: "supreme", password: "hemantsharma"
   # layout false
 end
