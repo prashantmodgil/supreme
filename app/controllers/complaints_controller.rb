51f322b200d032ad2637478535ba5ec677f21f44
parshantmodgil <prashant080994@gmail.com>
2019-07-01 09:57:33 +0530

    hot fix

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -78,13 +78,13 @@ class ComplaintsController < ApplicationController
      mec_name = params[:mechenic_search][:mec_name]
      start_date = params[:start_date].to_datetime
      stop_date = params[:stop_date].to_datetime
-     @complaints =Complaint.where("complaint_status like? AND mechenic like?","#{status}","#{mec_name}").where(call_date: start_date..stop_date).order("created_at DESC")
+     @complaints =Complaint.where("complaint_status like? AND mechenic like?","#{status}","#{mec_name}").where(call_date: start_date..stop_date).order("created_at DESC").page(params[:page])
    elsif params.has_key? (:dealer_search)
      status = params[:dealer_search][:complaint_status]
      dealer_name = params[:dealer_search][:dealer_name]
      start_date = params[:start_date].to_datetime
      stop_date = params[:stop_date].to_datetime
-    @complaints =Complaint.where("complaint_status like? AND dealer like?","#{status}","#{dealer_name}").where(call_date: start_date..stop_date).order("created_at DESC")
+    @complaints =Complaint.where("complaint_status like? AND dealer like?","#{status}","#{dealer_name}").where(call_date: start_date..stop_date).order("created_at DESC").page(params[:page])
    else
      @complaints = Complaint.all.order("created_at DESC").page(params[:page])
    end
