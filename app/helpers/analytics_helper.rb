196f7d0139ada822c0fa4121780a9fb168ca728a
parshantmodgil <prashant080994@gmail.com>
2019-08-30 00:16:02 +0530

    optimized mechenic wise data

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -12,10 +12,10 @@ module AnalyticsHelper
     Complaint.where("mechenic = ?", name ).count
   end
   def mec_solved_30 name
-    Complaint.where("mechenic = ?", name ).where("complaint_status = ?", "resolved"||"ok" ).count
+    Complaint.where("mechenic = ?", name ).where(complaint_status: ["resolved","ok"] ).count
   end
   def name_pending_total name
-    Complaint.where("mechenic = ?", name ).where("complaint_status != ?", "resolved"||"ok" ).count
+    Complaint.where("mechenic = ?", name ).where.not(complaint_status: ["resolved","ok"] ).count
   end
   def all_mechname
     Mechenic.all.pluck("mec_name")
