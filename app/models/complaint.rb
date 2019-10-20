7bcd50580dcbc9d50e39521f27d5904da017d2a9
parshantmodgil <prashant080994@gmail.com>
2019-10-20 22:41:11 +0530

    adding calback for uid

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,4 +1,5 @@
 class Complaint < ApplicationRecord
+  before_create :check_uid
   validates :mechenic,presence: true
   validates :createdby,presence: true
   validates :dealer,presence: true
@@ -6,9 +7,13 @@ class Complaint < ApplicationRecord
   validates :complaint_status,presence: true
   validates :warranty,presence: true
   validates :call_history,presence: true
+  validates :uid,presence: true
   # validates :site_address,presence: true
   # validates :coustomer_address,presence: true
   # validates :coustomer_phone,presence: true
   # validates :coustomer_phone, format: { with: /\d{10}/,message: "only allows 10 digits" }
    self.per_page = 10
+   def check_uid
+     self.uid = Complaint.last.uid + 1
+   end
 end
