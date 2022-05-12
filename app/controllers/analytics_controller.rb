b1991c71fd5c7cd0caebbabf0f3627849abe2254
prashant modgil <prashant080994@gmail.com>
2022-05-12 17:02:52 +0530

    some code changes

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,6 +1,6 @@
 class AnalyticsController < ApplicationController
   def index
-    @mechenics = Mechenic.all.pluck("mec_name")
+    # @mechenics = Mechenic.all.pluck("mec_name")
     @complaints = Complaint.all
     # @total_complaints = Complaint.where(created_at: (Time.now.midnight - 30.day)..Time.now.midnight).count
   end
--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -1,21 +1,21 @@
 module AnalyticsHelper
   def total_complaints
-    Complaint.count
-  end
+    @complaints.count
+  end 
   def resolved_complaints
-     Complaint.where(complaint_status: ["resolved","ok"] ).count
+     @complaints.where(complaint_status: ["resolved","ok"] ).count
   end
   def pending_complaints
-    Complaint.where.not(complaint_status: ["resolved","ok"] ).count
+    @complaints.where.not(complaint_status: ["resolved","ok"] ).count
   end
   def mec_30 name
-    Complaint.where("mechenic = ?", name ).count
+    @complaints.where("mechenic = ?", name ).count
   end
   def mec_solved_30 name
-    Complaint.where("mechenic = ?", name ).where(complaint_status: ["resolved","ok"] ).count
+    @complaints.where("mechenic = ?", name ).where(complaint_status: ["resolved","ok"] ).count
   end
   def name_pending_total name
-    Complaint.where("mechenic = ?", name ).where.not(complaint_status: ["resolved","ok"] ).count
+    @complaints.where("mechenic = ?", name ).where.not(complaint_status: ["resolved","ok"] ).count
   end
   def all_mechname
     Mechenic.all.pluck("mec_name")
