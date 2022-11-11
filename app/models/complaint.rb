b2d335fac979f852d7c3400e3e42ba9dd262ae72
prashant modgil <prashant080994@gmail.com>
2022-11-11 23:46:29 +0530

    ordering

--- app/models/complaint.rb
+++ app/models/complaint.rb
@@ -1,12 +1,12 @@
 class Complaint < ApplicationRecord
-  # before_create :check_uid
-  # validates :mechenic,presence: true
-  # validates :createdby,presence: true
-  # validates :dealer,presence: true
-  # validates :product_name,presence: true
-  # validates :complaint_status,presence: true
-  # validates :warranty,presence: true
-  # validates :call_history,presence: true
+  before_create :check_uid
+  validates :mechenic,presence: true
+  validates :createdby,presence: true
+  validates :dealer,presence: true
+  validates :product_name,presence: true
+  validates :complaint_status,presence: true
+  validates :warranty,presence: true
+  validates :call_history,presence: true
   # validates :uid,presence: true
   # validates :site_address,presence: true
   # validates :coustomer_address,presence: true
